44a7bfaa908b26f4d031ba9e033f3932ad0de60b
Quaresma <j.quaresmasantos_98@hotmail.com>
2017-11-13 22:12:06 -0300

    Aleluia

--- Neuronio_IC/src/main/Exe.java
+++ Neuronio_IC/src/main/Exe.java
@@ -5,28 +5,46 @@ import java.util.Scanner;
 public class Exe {
 
 	public static void main(String[] arguments) {
-		int[][] entry = { 
-						{ 1, 0, 1, 0, 0, 0},//Virus 
-						{ 0, 1, 0, 1, 0, 0},//Bacteria
-						{ 1, 0, 1, 1, 1, 0},//Dor de cabeça 
-						{ 0, 1, 0, 1, 1, 1} //Corisa
+		int[][] entry = { { 1, 0, 1, 0 }, 
+						  { 0, 1, 0, 1 }, 
+						  { 1, 0, 1, 1 }, 
+						  { 0, 1, 1, 1 }, 
+						  { 0, 0, 1, 1 },
+						  { 0, 0, 0, 1 }, 
 						};
-
-		@SuppressWarnings("resource")
+		int[] saidaAnd = { 0, 1, 0, 1, 0, 1 };
+			
+		Rede rede = new Rede();
+		rede.treinar(entry, saidaAnd);
+		
+		
 		Scanner entrada = new Scanner(System.in);
-		int op;
-		do {
-			System.out.println("Operacao:");
-			System.out.println("\t0 - AND");
-			System.out.println("\t1 - OR");
-			System.out.println("\t2 - XOR");
+		
+		int[] exemplo = new int[4];
 
-			op = entrada.nextInt();
+		System.out.println("Digite " + entry[0].length + " atributos: ");
+		
+		for (int i = 0; i < entry[0].length; i++) {
+			exemplo[i] = entrada.nextInt();
+		}
+		
+		int result = rede.executar(exemplo);
+		
+		if (result == 1) 
+			System.out.println("Resfriado");
+		else	
+			System.out.println("Gripe");
+		
+			
+		
 
-			if (op >= 0 || op <= 2) {
-				Rede rede = new Rede(op);
-				rede.treinar(entry);
-			}
-		} while (op != 3);
+		
+		//if(exemplo == entry[])
+			
+		
+		
+		
+		
 	}
+
 }
--- Neuronio_IC/src/main/Rede.java
+++ Neuronio_IC/src/main/Rede.java
@@ -1,86 +1,45 @@
 package main;
 
-import java.util.Arrays;
-import java.util.Random;
-
 public class Rede {
 
-	private Neuronio neuronio = new Neuronio(new double[3], 0, 0.1);
+	private int qtdEntradas;
+	private Neuronio neuronio;
 
-	private final int aux = -1;
-	private final int MAX_EPOCAS = 1000;
-	private int operacao = 0;
-	private Random rand = new Random();
+	public void treinar(int[][] entry, int[] saidaDesejada) {
+		double erros = 0;
 
-	public Rede(int op) {
+		qtdEntradas = entry[0].length;
+		// qtdExemplos = entry.length;
 
-		if ((op >= 0) && (op < 5)) {
-			operacao = op + 2;
-		}
-		// Carregando os pesos randomicamente
-		for (int i = 0; i < 3; i++) {
-			neuronio.getPeso()[i] = ((rand.nextInt(20) + 1) / 10) - 1;
-		}
+		neuronio = new Neuronio(qtdEntradas);
 
-	}
-
-	public int executar(int x1, int x2) {
-		// Somatorio da função
-		neuronio.setValorInicial(((aux) * neuronio.getPeso()[0]) + (x1 * neuronio.getPeso()[1]) + (x2 * neuronio.getPeso()[2]));
-		// Funcao de Transferencia
-		if (neuronio.getValorInicial() > 0) {
-			return 1;
-		}
-		return 0;
-	}
-
-	public boolean treinar(int[][] entry) {
-		boolean treinou;
-		int epoca = 0;
-		int erros = 0;
 		do {
-			treinou = true;
-			for (int i = 0; i < 4; i++) {
-				// Saida == s
-				int s = executar(entry[i][0], entry[i][1]);
-				int[][] s2;
-				s2 = entry;
-				if(i == 0)
-					System.out.println("Virus - " + Arrays.toString(s2[i]));
-				if(i == 1)
-					System.out.println("Bacteria - " + Arrays.toString(s2[i]));
-				if(i == 2)
-					System.out.println("Dor de Cabeça - " + Arrays.toString(s2[i]));
-				if(i == 3)
-					System.out.println("Corisa - " + Arrays.toString(s2[i]));
-			
-				System.out.println("Saida Posição("+ i + "): "+ s +" Epoca: "+ epoca);
-				// Compara com o que esta definido no main caso for diferente saida da entrada
-				// corrige o peso ate que se chegue no 100%
-				if (s != entry[i][operacao]) {
-					corrigirPeso(i, s, entry);
-					erros++; //Perguntar a Laura
-					treinou = false;
-				}
+			erros = 0;
+
+			for (int i = 0; i < entry.length; i++) {
+				neuronio.setEntry(entry[i]);
+				neuronio.calculaSaida();
+				neuronio.setErro(saidaDesejada[i]);
+
+				Math.abs(erros += neuronio.getErro());
+
+				//System.out.println(Math.abs(erros));
 			}
-			System.out.println("");
-			epoca++;
-		} while ((treinou == false) && (epoca < MAX_EPOCAS));
-		if(erros == 0)
-			System.out.println("O algoritmo treinou " + epoca + " epocas. Teve "+ erros + " erro(s). PERFECT!");
-		else	
-			System.out.println("O algoritmo treinou " + epoca + " epocas. Teve "+ erros + " erro(s).");
-		if (treinou == false) {
-			System.out.println("O algoritmo nao conseguiu convergir...");
-		}
-		return treinou;
+
+		} while (erros != 0);
+		
+		System.out.println("Erros totais: "+Math.abs(erros));
 	}
 
-	public void corrigirPeso(int exemplo, int saida, int[][] entry) {
-		neuronio.getPeso()[0] = neuronio.getPeso()[0] + (neuronio.getTaxaAprendiz() * (entry[exemplo][operacao] - saida) * (aux));
-		neuronio.getPeso()[1] = neuronio.getPeso()[1] + (neuronio.getTaxaAprendiz() * (entry[exemplo][operacao] - saida) * entry[exemplo][0]);
-		neuronio.getPeso()[2] = neuronio.getPeso()[2] + (neuronio.getTaxaAprendiz() * (entry[exemplo][operacao] - saida) * entry[exemplo][1]);
-		//System.out.println(neuronio.getPeso()[0] + " " + neuronio.getPeso()[1] + " " + neuronio.getPeso()[2]);
+	public int executar(int[] exemplos) {
+
+		neuronio.setEntry(exemplos);
+		neuronio.calculaSaida();
+
+		if (neuronio.getSaida() > 0)
+			return 1;
+		else
+			return 0;
 	}
 
 }
