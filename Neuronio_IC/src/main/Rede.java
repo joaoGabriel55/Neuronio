15c436f09e6d7dc04384e5c3a942acc6742fd0eb
João Gabriel Quaresma <j.quaresmasantos_98@hotmail.com>
2017-11-14 06:51:37 -0200

    Update Rede.java

--- Neuronio_IC/src/main/Rede.java
+++ Neuronio_IC/src/main/Rede.java
@@ -1,5 +1,10 @@
 package main;
-
+/**
+ A rede serve para que tudo que for apresentado ao neuronio seja
+ analisado de forma tal que ele possa aprender com os acertos 
+ e erro igual a um ser humano. Como se fosse um controlador se
+ analisamos pelo lado do MVC.
+**/
 public class Rede {
 
 	private int qtdEntradas;
@@ -7,15 +12,11 @@ public class Rede {
 
 	public void treinar(int[][] entry, int[] saidaDesejada) {
 		double erros = 0;
-
 		qtdEntradas = entry[0].length;
-		// qtdExemplos = entry.length;
-
 		neuronio = new Neuronio(qtdEntradas);
 
 		do {
 			erros = 0;
-
 			for (int i = 0; i < entry.length; i++) {
 				neuronio.setEntry(entry[i]);
 				neuronio.calculaSaida();
@@ -32,7 +33,6 @@ public class Rede {
 	}
 
 	public int executar(int[] exemplos) {
-
 		neuronio.setEntry(exemplos);
 		neuronio.calculaSaida();
 
